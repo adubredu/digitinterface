5b5bd3eae629a95bdde227938bbb343214a72a9e
Alphonsus Adu-Bredu <alphonsusbq436@gmail.com>
2023-04-06 17:00:35 -0400

    added installation instructions

--- README.md
+++ README.md
@@ -3,6 +3,13 @@
 Python package for interacting with the Agility Robotics Digit v3 default controller interface.
 
 ## Installation
+To install this package, run the following commands
+```
+git clone https://github.com/adubredu/digitinterface.git
+cd digitinterface
+pip install setuptools wheel ws4py json
+python setup.py sdist bdist_wheel
+```
 
 ## Usage
 Check the [examples](examples) folder for example usage. Check out the official [documentation](documentation) for more details.
---
+++ setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name="digitinterface",
+    version="0.1.0",
+    author="Alphonsus Adu-Bredu",
+    author_email="adubredu@umich.edu",
+    description="Python package for interacting with the Agility Robotics Digit v3 default controller interface.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/adubredu/digitinterface",
+    packages=find_packages(),
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+    ],
+    python_requires='>=3.6',
+    install_requires=[
+        # Add your package's dependencies here, e.g.,
+        "numpy>=1.18.0",
+        "ws4py",
+        "json"
+    ],
+)
